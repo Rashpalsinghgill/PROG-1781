1d32559e54222862e47f0915d8087586afc3d8e4
Rashpalsinghgill <43342689+Rashpalsinghgill@users.noreply.github.com>
2018-11-13 18:33:11 +0530

    Add files via upload

---
+++ A2RashpalSinghP1/A2RashpalSinghP1/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A2RashpalSinghP1
+{
+    class Program
+    { 
+        static void Main(string[] args)
+        {
+            double c,d;
+            Console.WriteLine("Student's age");
+            string ag = Console.ReadLine();
+            double age = double.Parse(ag);
+            Console.WriteLine("International Student (yes,no)");
+            string a = Console.ReadLine();
+            Console.WriteLine("Registration Semester (fall, winter, summer)");
+            string b = Console.ReadLine();
+
+            if(a=="no")
+            {
+                if(age<=18)
+                {
+                    c = 300 + (0.13 * 300);
+                    Console.WriteLine("Base Tuition $ " + c);
+                }
+                else if(age<=49)
+                {
+                    c = 500 + (0.13 * 300);
+                    Console.WriteLine("Base Tuition $ " + c);
+                }
+                else
+                {
+                    c = 400 + (0.13 * 400);
+                    Console.WriteLine("Base Tuition $ " + c);
+                }
+            }
+            else if(a=="yes")
+            {
+                if(age<=18)
+                {
+                    c = 400 + (0.13 * 400);
+                    Console.WriteLine("International Student fee " + c);
+                }
+                else if(age<=49)
+                {
+                    c=600+(0.13*600);
+                    Console.WriteLine("International Student fee " + c);
+                }
+                else
+                {
+                    c = 500 + (0.13 * 500);
+                    Console.WriteLine("International Student fee " + c);
+                }
+            }
+            switch(b)
+            {
+                case "fall":
+                    {
+                        d = 250 + (0.13 * 250);
+                        Console.WriteLine("Registration fee for semester $ " + d);
+                        double tax = 250 * 0.13;
+                        Console.WriteLine("HST $ " + tax);
+                        double total = d + tax;
+                        Console.WriteLine("total: " + total);
+                        break;
+                    }
+                case "winter":
+                    {
+                        d = 220 + (0.13 * 220);
+                        Console.WriteLine("Registration fee for semester " + d);
+                        double tax = 220 * 0.13;
+                        Console.WriteLine("HST $ " + tax);
+                        double total = d + tax;
+                        Console.WriteLine("total: " + total);
+                        break;
+                    }
+                case "summer":
+                    {
+                        d = 150 + (0.13 * 150);
+                        Console.WriteLine("Registration fee for semester " + d);
+                        double tax = 150 * 0.13;
+                        Console.WriteLine("HST $ " + tax);
+                        double total = d + tax;
+                        Console.WriteLine("total: " + total);
+                        break;
+                    }
+                default:
+                    {
+                        Console.WriteLine("Enter valid month ");
+                        break;
+                    }
+            }
+        }
+    }
+}
---
+++ A2RashpalsinghP2/A2RashpalsinghP2/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A2RashpalsinghP2
+{
+    class Program
+    {
+        static double seq(double r, double pi)
+        {
+            double seq;
+            seq = 4 / 3 * pi * r * r * r;
+            return seq;
+        }
+        static double seq(double rad, double h, double pi)
+        {
+            double seq;
+            seq = pi * rad * rad * h;
+            return seq;
+        }
+        static int seq(int l, int b, int h)
+        {
+            int seq;
+            seq = l * b * h;
+            return seq;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Press 1 for sphere ");
+            Console.WriteLine("2 for cylinder ");
+            Console.WriteLine("3 for rectangular prism");
+            string x = Console.ReadLine();
+            int y = int.Parse(x);
+
+            if (y == 1)
+            {
+                Console.WriteLine("enter the value of radius");
+                string o = Console.ReadLine();
+                double r = double.Parse(o);
+                double result;
+                result = seq(3.14, r);
+                Console.WriteLine(" volume of sphere is = " + result);
+            }
+
+            else if (y == 2)
+            {
+                Console.WriteLine("enter the value of radius");
+                string j = Console.ReadLine();
+                double c = double.Parse(j);
+                Console.WriteLine("enter the value of height");
+                string m = Console.ReadLine();
+                double n = double.Parse(m);
+                double res;
+                res = seq(3.14, c, n);
+                Console.WriteLine(" volume of cylinder is = " + res);
+            }
+
+            else if (y == 3)
+            {
+                Console.WriteLine("enter the value of length");
+                string g = Console.ReadLine();
+                int z = int.Parse(g);
+                Console.WriteLine("enter the value of breadth");
+                string v = Console.ReadLine();
+                int b = int.Parse(v);
+                Console.WriteLine("enter the value of height");
+                string r = Console.ReadLine();
+                int h = int.Parse(r);
+                int result;
+                result = seq(z, b, h);
+                Console.WriteLine(" volume of rectangular prism is = " + result);
+            }
+        }
+    }
+}
